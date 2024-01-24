d6031c650589aef6820e78d092cf75f99eb80998
cobrianz <briancheruiyot.h@gmail.com>
2024-01-24 21:59:39 +0300

    first commit

---
+++ src/components/about/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './about.css'
+import aboutData from '../../data/aboutData'
+import ProfileImageUrl from '../../assets/profile-1.png'
+const about = () => {
+  return (
+    <div className='about'>
+    
+    </div>
+  )
+}
+
+export default about
---
+++ src/data/aboutData.js
@@ -0,0 +1,22 @@
+const aboutData = {
+    titleSmall: "Get to Know more",
+    title: "About Me",
+    aboutText: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet optio nobis suscipit cupiditate, laudantium culpa earum repellendus  provident dolorem maiores officiis hic! Repellendus eligendi neque ad  voluptate iste eveniet. Sed quaerat iste maiores minima similique mollitia consequatur alias repellat atque ex, explicabo tenetur delectus error  consectetur porro ab molestias cum!",
+    about: [
+        { 
+            Image : "'../../assets/experience.png'",
+            label: 'Experience',
+            year: '2+ years',
+            work: 'Frontend Development' 
+
+        },
+        {   Image: '../../assets/education.png',
+            label: 'Education',
+            year: 'B.Sc Computer Science',
+            work: 'Maseno University' 
+        },
+    
+    ]
+};
+
+export default aboutData;
