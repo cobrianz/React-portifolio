8c71776751d430171e786518b4470ce5e0595f13
cobrianz <briancheruiyot.h@gmail.com>
2024-01-27 16:58:23 +0300

    first commit

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './hero.css'
+import profile from '../../assets/profile.png'
+import github from '../../assets/github.png'
+import linkedin from '../../assets/linkedin.png'
+import HeroData from '../../data/heroData'
 const hero = () => {
   return (
-    <div>hero</div>
+    <div className='hero'>
+      <img src={profile} alt="" />
+      <div className="hero__details">
+      <div className="hero__detail">
+        {HeroData.map((element, index) => (
+          <span key={index}>{element}</span>
+        ))}
+      </div>
+      <div className="social__links">
+      <a href="#"><img src={linkedin} alt="" /></a>
+      <a href="#"><img src={github} alt="" /></a>
+      </div>
+      </div>
+    </div>
   )
 }
 
